16f96db639552403c48fb57fe34501d116702308
Adel Mahjoub <dragonhunter250@gmail.com>
2017-07-15 12:37:02 +0200

    first commit

---
+++ server.js
@@ -0,0 +1,18 @@
+require('dotenv').config();
+const express = require('express');
+const helmet = require('helmet');
+const path = require('path');
+const nodeCache = require('node-cache');
+
+const app = express();
+
+app.set('port', process.env.PORT || 3000);
+app.use(express.static(path.join(__dirname, 'public')));
+
+app.get('/', (req, res, next) => {
+  res.json({status: 'ok'});
+});
+
+app.listent(app.get('port'), () => {
+  console.log(`Server running on port ${app.get('port')}`);
+});
